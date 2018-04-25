f10a790335d602270835af34a32139a50d5aab80
renjithsraj <renjithsraj@live.com>
2018-04-26 00:28:41 +0530

    added data

--- setup.py
+++ setup.py
@@ -44,6 +44,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/cadmus/paywix',
-    version='0.1.1',
+    version='1.0.1',
     zip_safe=False,
 )
