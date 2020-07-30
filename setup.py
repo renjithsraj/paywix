ab579ef7d63a2ccaabadd2b88b57051fe0759b88
renjithsraj <renjithsraj@live.com>
2020-07-30 14:56:13 +0530

    Update readme files

--- setup.py
+++ setup.py
@@ -9,11 +9,14 @@ with open(path.join(p, 'README.rst')) as f:
 
 setup(
     name='paywix',
-    version='1.2.0',
-    description='Multipayment gateway wrapper for Django',
+    version='1.3.0',
+    author="Renjith S Raj",
+    author_email="renjithsraj@live.com",
+    description='Paywix is a light weight payment processing sdk for python based applications.',
     long_description=README,
     long_description_content_type='text/markdown',
     install_requires=[
+        'requests'
     ],
     url='https://github.com/renjithsraj/paywix',
     maintainer='Renjith S Raj',
@@ -22,9 +25,15 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     classifiers=[
-        "Programming Language :: Python :: 3",
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3 :: Only',
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    keywords='payment processing, payu, python payment gateway wrapper, cashfree, paytm, etc',
 )
